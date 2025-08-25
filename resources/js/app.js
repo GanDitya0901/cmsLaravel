af8746de966bcf8986b380c3685f29dfed74fe3d
Gana Aditya <gana@itsourcebali.com>
2025-08-25 16:07:45 +0800

    Add Modal for  Post and Category

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,7 @@
 import './bootstrap';
 import './page-editor';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- resources/views/post/showAllPost.blade.php
+++ resources/views/post/showAllPost.blade.php
@@ -86,13 +86,44 @@
                                 class="mx-2 p-2 bg-amber-300 rounded-md hover:bg-amber-400 hover:text-white">View</a>
                             <a href="{{ route('show.editForm', $data->id) }}"
                                 class="mx-2 p-2 bg-amber-300 rounded-md hover:bg-amber-400 hover:text-white">Update</a>
-                            <form action="{{ route('deletePost', $data->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
+                            <div x-data="{ openModal: false }">
+                                <button @click="openModal = true"
+                                    class="mx-2 p-2 bg-red-500 text-white rounded-md cursor-pointer hover:bg-red-700">Delete
+                                </button>
 
-                                <button
-                                    class="mx-2 p-2 bg-red-500 text-white rounded-md cursor-pointer hover:bg-red-700">Delete</button>
-                            </form>
+                                {{-- Modal --}}
+                                <div x-show="openModal"
+                                    class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900/50">
+                                    <div class="flex w-96 flex-col items-center gap-3 rounded-md bg-white p-6 shadow-md"
+                                        @click.away="openModal = false">
+                                        <h1 class="font-bold">Are you sure?</h1>
+                                        <div class="flex items-center justify-center rounded-full bg-red-500 p-3">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                                class="h-15 w-15 fill-white">
+                                                <path fill-rule="evenodd"
+                                                    d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                        <p class="text-gray-500">This action will permanently delete the post</p>
+                                        <div class="flex gap-5">
+                                            <button @click="openModal = false"
+                                                class="bg-gray-400 text-white py-1 px-3 rounded-md cursor-pointer hover:bg-gray-500">
+                                                Cancel
+                                            </button>
+                                            <form action="{{ route('deletePost', $data->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+
+                                                <button
+                                                    class="bg-red-500 py-1 px-3 text-white rounded-md cursor-pointer hover:bg-red-600">
+                                                    Confirm
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
@@ -184,19 +215,50 @@
                             <img src="{{ asset($data->filename) }}" alt="Post Image"
                                 class="max-w-full max-h-[50px] object-contain">
                         </td>
-                        <td>{{ $data->created_at }}</td>    
+                        <td>{{ $data->created_at }}</td>
                         <td class="flex items-center">
                             <a href="{{ route('show.post', $data->id) }}"
                                 class="mx-2 p-2 bg-amber-300 rounded-md hover:bg-amber-400 hover:text-white">View</a>
                             <a href="{{ route('show.editForm', $data->id) }}"
                                 class="mx-2 p-2 bg-amber-300 rounded-md hover:bg-amber-400 hover:text-white">Update</a>
-                            <form action="{{ route('deletePost', $data->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
+                            <div x-data="{ openModal: false }" class="cursor-pointer">
+                                <button @click="openModal = true"
+                                    class="mx-2 p-2 bg-red-500 text-white rounded-md cursor-pointer hover:bg-red-700">Delete
+                                </button>
 
-                                <button
-                                    class="mx-2 p-2 bg-red-500 text-white rounded-md cursor-pointer hover:bg-red-700">Delete</button>
-                            </form>
+                                {{-- Modal --}}
+                                <div x-show="openModal"
+                                    class="fixed inset-0 z-50 flex items-center justify-center">
+                                    <div class="flex w-96 flex-col items-center gap-3 rounded-md bg-white p-6 shadow-md"
+                                        @click.away="openModal = false">
+                                        <h1 class="font-bold">Are you sure?</h1>
+                                        <div class="flex items-center justify-center rounded-full bg-red-500 p-3">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                                class="h-15 w-15 fill-white">
+                                                <path fill-rule="evenodd"
+                                                    d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                        <p class="text-gray-500">This action will permanently delete the post</p>
+                                        <div class="flex gap-5">
+                                            <button @click="openModal = false"
+                                                class="bg-gray-400 text-white py-1 px-3 rounded-md cursor-pointer hover:bg-gray-500">
+                                                Cancel
+                                            </button>
+                                            <form action="{{ route('deletePost', $data->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+
+                                                <button
+                                                    class="bg-red-500 py-1 px-3 text-white rounded-md cursor-pointer hover:bg-red-600">
+                                                    Confirm
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </td>
                     </tr>
                 @endforeach
